ab5deb69e15e7b0fd444a11a1f0365a9c07f5ae0
rmamaral <rm.amaral@campus.fct.unl.pt>
2018-03-29 02:46:32 +0100

    keystore implementation

--- src/main/java/fct/srsc/stgc/phase1/STGCMulticastSocket.java
+++ src/main/java/fct/srsc/stgc/phase1/STGCMulticastSocket.java
@@ -7,22 +7,23 @@ import javax.crypto.Cipher;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.MulticastSocket;
 import java.net.SocketAddress;
+import java.security.Key;
+import java.security.KeyStore;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.util.Arrays;
+import java.util.Base64;
 
 public class STGCMulticastSocket extends MulticastSocket {
 
-    public final static int BLOCKSIZE = 64;
-    private final SecretKey key64 = new SecretKeySpec(new byte[]{
-            0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07}, "blowfish");
-
-    ChatRoomConfig config;
-    Cipher c;
+    private ChatRoomConfig config;
+    private Cipher c;
 
     public STGCMulticastSocket(String groupAddress) throws IOException, NoSuchPaddingException, NoSuchAlgorithmException, NoSuchProviderException {
         super();
@@ -41,12 +42,39 @@ public class STGCMulticastSocket extends MulticastSocket {
         config = ReadFromConfig.readFromConfig(groupAddress);
         c = Cipher.getInstance(config.getCiphersuite(), config.getProvider());
     }
+    
+    public Key getKeyFromKeyStore(String type, String keystore, String key, char[] keyPassword, char[] keyStorePassword) {
+
+    	try {
+    	KeyStore keyStore = KeyStore.getInstance(type);
+        	   // Keystore where symmetric keys are stored (type JCEKS)
+   	    FileInputStream stream = new FileInputStream(keystore);
+               // Password of the keystore: in ths case is "password"
+   	    keyStore.load(stream, keyStorePassword);
+
+               // We decided to store the keys in these entries: mykey1, mykey2
+               // Also, we used "password" to protect each entry...
+               // See how the keytool was used ...
+
+   	    Key key1 = keyStore.getKey(key, keyPassword);
+   	    System.out.println(Base64.getEncoder().encodeToString(key1.getEncoded()));
+   	    return key1;
+   	    
+    	}
+    	catch(Exception e) {
+    		return null;
+    	}
+    	
+    }
 
     @Override
     public void send(DatagramPacket packet) throws IOException {
         System.out.println("Sending message through secure channel");
 
         try {
+        	
+        	Key key64 = getKeyFromKeyStore("JCEKS", "mykeystore.jks", "mykey1", "password".toCharArray(), "password".toCharArray());
+        
             c.init(Cipher.ENCRYPT_MODE, key64);
             byte[] enc = c.doFinal(packet.getData());
 
@@ -69,7 +97,7 @@ public class STGCMulticastSocket extends MulticastSocket {
             DatagramPacket p = new DatagramPacket(new byte[65356], 65356);
 
             super.receive(p);
-
+            Key key64 = getKeyFromKeyStore("JCEKS", "mykeystore.jks", "mykey1", "password".toCharArray(), "password".toCharArray());
             c.init(Cipher.DECRYPT_MODE, key64);
             byte[] enc = c.doFinal(Arrays.copyOf(p.getData(), p.getLength()));
 
